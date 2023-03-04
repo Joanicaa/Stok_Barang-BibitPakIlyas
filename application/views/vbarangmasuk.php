0ba7a59aa33e530faf5d8deeef9e8aee2de9e27f
joanicaa <joanica.gifta25@gmail.com>
2023-03-04 18:48:17 +0700

    merubah modal

--- application/views/vbarangmasuk.php
+++ application/views/vbarangmasuk.php
@@ -33,29 +33,6 @@
 </div>
 <!-- end modal Tambah barang -->
 
-<!-- start modal Tambah barang -->
-<button type="button" class="btn btn-danger mb-3" data-bs-toggle="modal" data-bs-target="#KurangiBarang"><i class="bi bi-file-earmark-plus"></i>
-  Kurangi Barang </button>
-
-<div class="modal fade" id="KurangiBarang" tabindex="-1">
-  <div class="modal-dialog modal-lg">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h5 class="modal-title">Coba</h5>
-        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-      </div>
-      <div class="modal-body">
-        Non omnis incidunt qui sed occaecati magni asperiores est mollitia. Soluta at et reprehenderit. Placeat autem numquam et fuga numquam. Tempora in facere consequatur sit dolor ipsum. Consequatur nemo amet incidunt est facilis. Dolorem neque recusandae quo sit molestias sint dignissimos.
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-        <button type="button" class="btn btn-primary">Save changes</button>
-      </div>
-    </div>
-  </div>
-</div>
-<!-- end modal Tambah barang -->
-
 
 <div class="card">
   <div class="card-body">
